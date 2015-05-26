84190cb259baa2e72ce401b1fe893191bba55449
alvaro <alvaroabascar@gmail.com>
2015-05-26 10:58:10 +0200

    gene enrichment

---
+++ enrichment_analysis.R
@@ -0,0 +1,23 @@
+library(GSEABase)
+library(affyPLM)
+library(Biobase)
+library(genefilter)
+
+glioData = readRDS('rma_glioData.rds')
+eset = readRDS('eset.rds')
+
+g28_ids = as.character(read.csv('cluster_g28.csv', header=FALSE)$V3)
+
+g28 = GeneSet(EntrezIdentifier("org.Hs.eg.db"), geneIds=g28_ids, setName="G28")
+details(g28)
+
+cluster_collection = GeneSetCollection(g28)
+cluster_collection
+length(cluster_collection)
+head(names(cluster_collection))
+
+cluster_collection = mapIdentifiers(cluster_collection, AnnoOrEntrezIdentifier(annotation(eset)))
+Im = incidence(cluster_collection)
+
+
+GSeset = gsva(eset, cluster_collection, min.sz=10, max.sz=500, verbose=FALSE)$es.obs
