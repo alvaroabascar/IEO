39ac704093936d2dfae5b524fd6969ec77eb92ed
Eloi <eloi@example.com>
2015-05-26 11:03:25 +0200

    geneset with hgu133plus2.db annotation

--- enrichment_analysis.R
+++ enrichment_analysis.R
@@ -2,13 +2,15 @@ library(GSEABase)
 library(affyPLM)
 library(Biobase)
 library(genefilter)
+library(hgu133plus2.db)
 
 glioData = readRDS('rma_glioData.rds')
 eset = readRDS('eset.rds')
+annotation(eset)<-'hgu133plus2.db'
 
 g28_ids = as.character(read.csv('cluster_g28.csv', header=FALSE)$V3)
 
-g28 = GeneSet(EntrezIdentifier("org.Hs.eg.db"), geneIds=g28_ids, setName="G28")
+g28 = GeneSet(AnnotationIdentifier("hgu133plus2.db"), geneIds=g28_ids, setName="G28")
 details(g28)
 
 cluster_collection = GeneSetCollection(g28)
